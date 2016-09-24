d7ef5f4ed70bacd4ea3f37fd180f21d2e0191a32
jalalahmad <jalal.ahmad@gmail.com>
2016-09-24 17:55:24 -0400

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'chef-fluentd'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef-fluentd'
+long_description 'Installs/Configures chef-fluentd'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/chef-fluentd/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/chef-fluentd' if respond_to?(:source_url)
